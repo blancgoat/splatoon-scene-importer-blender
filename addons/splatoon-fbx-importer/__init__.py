1f5c89a5d4ea21e1b6711f9f3b8bfebd9a87efe0
blancgoat <blancgoat@gmail.com>
2025-01-08 09:27:25 +0900

    class

--- addons/splatoon-fbx-importer/__init__.py
+++ addons/splatoon-fbx-importer/__init__.py
@@ -11,87 +11,64 @@ bl_info = {
 import bpy
 import os
 
-def link_texture(material, texture_dir, base_name, suffix, input_name, non_color=False, is_normal_map=False):
-    """Generic function to link a texture to a material input."""
-    texture_path = os.path.join(texture_dir, f"{base_name}{suffix}.png")
-    if not os.path.exists(texture_path):
-        return
+class MaterialProcessor:
+    def __init__(self, material):
+        self.material = material
+        self.principled_node = self._find_principled_node()
+        
+    def _find_principled_node(self):
+        """Find the Principled BSDF node in the material."""
+        for node in self.material.node_tree.nodes:
+            if node.type == 'BSDF_PRINCIPLED':
+                return node
+        return None
 
-    # Find the Principled BSDF node
-    principled_node = None
-    for node in material.node_tree.nodes:
-        if node.type == 'BSDF_PRINCIPLED':
-            principled_node = node
-            break
+    def set_metallic_value(self, value):
+        """Set the metallic value for the material."""
+        if self.principled_node:
+            self.principled_node.inputs['Metallic'].default_value = value
 
-    if not principled_node:
-        return
+    def link_texture(self, texture_dir, base_name, suffix, input_name, non_color=False, is_normal_map=False):
+        """Link a texture to a material input."""
+        if not self.principled_node:
+            return
 
-    # Create a new image texture node
-    tex_image_node = material.node_tree.nodes.new('ShaderNodeTexImage')
-    tex_image_node.image = bpy.data.images.load(texture_path)
-    if non_color:
-        tex_image_node.image.colorspace_settings.name = 'Non-Color'
+        texture_path = os.path.join(texture_dir, f"{base_name}{suffix}.png")
+        if not os.path.exists(texture_path):
+            return
 
-    if is_normal_map:
-        # Create and connect a normal map node
-        normal_map_node = material.node_tree.nodes.new('ShaderNodeNormalMap')
-        material.node_tree.links.new(tex_image_node.outputs['Color'], normal_map_node.inputs['Color'])
-        material.node_tree.links.new(normal_map_node.outputs['Normal'], principled_node.inputs[input_name])
-    else:
-        # Directly connect the texture to the specified input
-        material.node_tree.links.new(tex_image_node.outputs['Color'], principled_node.inputs[input_name])
+        # Create a new image texture node
+        tex_image_node = self.material.node_tree.nodes.new('ShaderNodeTexImage')
+        tex_image_node.image = bpy.data.images.load(texture_path)
+        if non_color:
+            tex_image_node.image.colorspace_settings.name = 'Non-Color'
 
-def handle_alpha_connection(material, texture_dir, base_name):
-    """Handle the alpha connection, removing existing links and connecting _Opa."""
-    # Find the Principled BSDF node
-    principled_node = None
-    for node in material.node_tree.nodes:
-        if node.type == 'BSDF_PRINCIPLED':
-            principled_node = node
-            break
+        if is_normal_map:
+            # Create and connect a normal map node
+            normal_map_node = self.material.node_tree.nodes.new('ShaderNodeNormalMap')
+            self.material.node_tree.links.new(tex_image_node.outputs['Color'], normal_map_node.inputs['Color'])
+            self.material.node_tree.links.new(normal_map_node.outputs['Normal'], self.principled_node.inputs[input_name])
+        else:
+            # Directly connect the texture to the specified input
+            self.material.node_tree.links.new(tex_image_node.outputs['Color'], self.principled_node.inputs[input_name])
 
-    if not principled_node:
-        return
+    def handle_alpha_connection(self, texture_dir, base_name):
+        """Handle the alpha connection, removing existing links and connecting _Opa."""
+        if not self.principled_node:
+            return
 
-    # Remove existing alpha links unconditionally
-    for link in material.node_tree.links:
-        if link.to_node == principled_node and link.to_socket.name == "Alpha":
-            material.node_tree.links.remove(link)
+        # Remove existing alpha links unconditionally
+        for link in self.material.node_tree.links:
+            if link.to_node == self.principled_node and link.to_socket.name == "Alpha":
+                self.material.node_tree.links.remove(link)
 
-    # Check for _Opa texture and connect if available
-    alpha_path = os.path.join(texture_dir, f"{base_name}_Opa.png")
-    if os.path.exists(alpha_path):
-        tex_image_node = material.node_tree.nodes.new('ShaderNodeTexImage')
-        tex_image_node.image = bpy.data.images.load(alpha_path)
-        tex_image_node.image.colorspace_settings.name = 'Non-Color'
-        material.node_tree.links.new(tex_image_node.outputs['Color'], principled_node.inputs['Alpha'])
-
-def process_imported_fbx(file_name):
-    """Process all imported meshes and armature."""
-    for obj in bpy.context.selected_objects:
-        if obj.type == 'MESH':
-            for mat_slot in obj.material_slots:
-                if mat_slot.material and mat_slot.material.use_nodes:
-                    # Detect the texture directory and base name
-                    nodes = mat_slot.material.node_tree.nodes
-                    for node in nodes:
-                        if node.type == 'TEX_IMAGE' and node.image:
-                            image_path = bpy.path.abspath(node.image.filepath)
-                            texture_dir = os.path.dirname(image_path)
-                            base_name = os.path.basename(image_path).split('_Alb')[0]
-
-                            # Link textures
-                            link_texture(mat_slot.material, texture_dir, base_name, "_Mlt", "Metallic", non_color=True)
-                            link_texture(mat_slot.material, texture_dir, base_name, "_Rgh", "Roughness", non_color=True)
-                            link_texture(mat_slot.material, texture_dir, base_name, "_Nrm", "Normal", non_color=True, is_normal_map=True)
-                            handle_alpha_connection(mat_slot.material, texture_dir, base_name)
-
-                            break
-        elif obj.type == 'ARMATURE':
-            # Set armature scale to (1, 1, 1)
-            obj.scale = (1.0, 1.0, 1.0)
-            obj.name = file_name
+        # Check for _Opa texture and connect if available
+        alpha_path = os.path.join(texture_dir, f"{base_name}_Opa.png")
+        if os.path.exists(alpha_path):
+            tex_image_node = self.material.node_tree.nodes.new('ShaderNodeTexImage')
+            tex_image_node.image = bpy.data.images.load(alpha_path)
+            tex_image_node.image.colorspace_settings.name = 'Non-Color'
+            self.material.node_tree.links.new(tex_image_node.outputs['Color'], self.principled_node.inputs['Alpha'])
 
 class IMPORT_OT_splatoon_fbx(bpy.types.Operator):
     """Custom Splatoon FBX Importer"""
@@ -104,11 +81,11 @@ class IMPORT_OT_splatoon_fbx(bpy.types.Operator):
     def execute(self, context):
         # Get the file name without extension
         file_name = os.path.splitext(os.path.basename(self.filepath))[0]
-
+        
         # Call the default FBX importer
         bpy.ops.import_scene.fbx(filepath=self.filepath)
 
-        # Process the imported FBX
+        # Register timer with the file name parameter
         bpy.app.timers.register(lambda: process_imported_fbx_after_delay(file_name))
 
         return {'FINISHED'}
@@ -120,7 +97,32 @@ class IMPORT_OT_splatoon_fbx(bpy.types.Operator):
 def process_imported_fbx_after_delay(file_name):
     """Wait until FBX import finishes before processing meshes."""
     if bpy.context.selected_objects:
-        process_imported_fbx(file_name)
+        for obj in bpy.context.selected_objects:
+            if obj.type == 'ARMATURE':
+                obj.scale = (1.0, 1.0, 1.0)
+                obj.name = file_name
+            elif obj.type == 'MESH':
+                for mat_slot in obj.material_slots:
+                    if mat_slot.material and mat_slot.material.use_nodes:
+                        material_processor = MaterialProcessor(mat_slot.material)
+                        
+                        # Set initial metallic value to 0
+                        material_processor.set_metallic_value(0.0)
+
+                        nodes = mat_slot.material.node_tree.nodes
+                        for node in nodes:
+                            if node.type == 'TEX_IMAGE' and node.image:
+                                image_path = bpy.path.abspath(node.image.filepath)
+                                texture_dir = os.path.dirname(image_path)
+                                base_name = os.path.basename(image_path).split('_Alb')[0]
+
+                                # Link textures using the material processor
+                                material_processor.link_texture(texture_dir, base_name, "_Mlt", "Metallic", non_color=True)
+                                material_processor.link_texture(texture_dir, base_name, "_Rgh", "Roughness", non_color=True)
+                                material_processor.link_texture(texture_dir, base_name, "_Nrm", "Normal", non_color=True, is_normal_map=True)
+                                material_processor.handle_alpha_connection(texture_dir, base_name)
+
+                                break
         return None  # Stop the timer
     return 0.1  # Retry after 0.1 seconds
 
