7bc87e21212394e7e85e4ddca31cfef6204ff667
blancgoat <blancgoat@gmail.com>
2025-01-18 16:43:05 +0900

    변수명정리

--- addons/splatoon-scene-importer/importers/splatoon/queueing.py
+++ addons/splatoon-scene-importer/importers/splatoon/queueing.py
@@ -12,29 +12,31 @@ class Queueing:
         self.imported_objects.clear()
 
         for file_elem in files:
-            filepath = os.path.join(directory, file_elem.name)
-            file_path = os.path.dirname(filepath)
-            file_name = os.path.splitext(file_elem.name)[0]
-            self.processing_queue.append((filepath, file_path, file_name))
+            file_path = os.path.join(directory, file_elem.name)
+            dir_path = os.path.dirname(file_path)
+            file_splitext = os.path.splitext(file_elem.name)
+            file_name = file_splitext[0]
+            file_ext = file_splitext[1].lower()
+            self.processing_queue.append((file_path, dir_path, file_name, file_ext))
 
     def process_next_file(self):
         """Process the next file in queue, including import and post-processing"""
 
         # If we have current objects to process
         if self.imported_objects:
-            current_file_name, current_objects, current_file_path = self.imported_objects[0]
+            objects, file_name, file_path = self.imported_objects[0]
 
             # Process current objects
-            for obj in current_objects:
+            for obj in objects:
                 if obj.type == 'ARMATURE':
                     if bpy.context.scene.is_scale_armature_splatoon_scene_importer:
                         scale = bpy.context.scene.scale_value_splatoon_scene_importer
                         obj.scale = (scale, scale, scale)
-                    obj.name = current_file_name
+                    obj.name = file_name
                 elif obj.type == 'MESH':
                     for mat_slot in obj.material_slots:
                         if mat_slot.material and mat_slot.material.use_nodes:
-                            material_processor = MaterialProcessor(mat_slot.material, current_file_path)
+                            material_processor = MaterialProcessor(mat_slot.material, file_path)
 
                             # metallic to 0
                             material_processor.principled_node.inputs['Metallic'].default_value = 0
@@ -67,21 +69,20 @@ class Queueing:
 
         # If we have more files to process
         if self.processing_queue:
-            filepath, file_path, file_name = self.processing_queue.popleft()
+            file_path, dir_path, file_name, file_ext = self.processing_queue.popleft()
 
             # Store currently selected objects
             prev_selected = set(obj.name for obj in bpy.context.selected_objects)
 
             # Import file
-            ext = os.path.splitext(filepath)[1].lower()
-            if ext == '.fbx':
-                bpy.ops.import_scene.fbx(filepath=filepath)
-            elif ext == '.dae':
+            if file_ext == '.fbx':
+                bpy.ops.import_scene.fbx(filepath=file_path)
+            elif file_ext == '.dae':
                 try:
-                    filepath = DAE_OT_import_via_fbx.convert(filepath)
-                    bpy.ops.import_scene.fbx(filepath=filepath)
+                    file_path = DAE_OT_import_via_fbx.convert(file_path)
+                    bpy.ops.import_scene.fbx(filepath=file_path)
                     try:
-                        os.unlink(filepath)
+                        os.unlink(file_path)
                     except:
                         pass
                 except (NotFoundConvertModule, FailConvert) as e:
@@ -89,7 +90,7 @@ class Queueing:
 
             # Find newly imported objects
             new_objects = [obj for obj in bpy.context.selected_objects if obj.name not in prev_selected]
-            self.imported_objects.append((file_name, new_objects, file_path))
+            self.imported_objects.append((new_objects, file_name, dir_path))
 
             return 0.1  # Continue timer for next file
 
